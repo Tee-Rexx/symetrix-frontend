3cf86b3d383b618846e8ab9bc823377993077ecf
Taha Shahzad <shahzadtaha638@gmail.com>
2025-01-31 03:54:51 +0500

    seo work done

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     experimental: {
       appDir: true,
     },
+    async rewrites() {
+        return [
+          {
+            source: '/sitemap.xml',
+            destination: '/api/sitemap', // The API route we just created
+          },
+        ];
+      },
   };
   
   export default nextConfig;
